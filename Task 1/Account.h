6d865169f8d3213e9bfb6ace3a1bf2737c3434a9
Sintelaris <Sintelaris@gmail.com>
2021-10-05 20:55:11 +0600

    Initial commit

---
+++ Task 1/Account.h
@@ -0,0 +1,14 @@
+//
+// Created by Sintelaris on 05.10.2021.
+//
+
+#ifndef TASK_1_ACCOUNT_H
+#define TASK_1_ACCOUNT_H
+
+
+class Account {
+
+};
+
+
+#endif //TASK_1_ACCOUNT_H
---
+++ Task 1/cmake-build-debug/CMakeFiles/Task_1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Task_1.dir/Account.cpp.obj"
+  "CMakeFiles/Task_1.dir/main.cpp.obj"
+  "Task_1.exe"
+  "Task_1.exe.manifest"
+  "Task_1.lib"
+  "Task_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Task_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
